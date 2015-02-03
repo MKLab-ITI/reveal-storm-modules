1846dc2fc0bdabc2d4309c730a9c231e67a45ffd
kandreadou <kandreadou@iti.gr>
2015-02-03 12:21:51 +0200

    Checking maven project structure and gitignore

---
+++ src/main/java/gr/iti/mklab/VisualIndexer.java
@@ -0,0 +1,7 @@
+package gr.iti.mklab;
+
+/**
+ * Created by kandreadou on 2/3/15.
+ */
+public class VisualIndexer {
+}
---
+++ src/main/java/gr/iti/mklab/bolts/IndexingBolt.java
@@ -0,0 +1,29 @@
+package gr.iti.mklab.bolts;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Tuple;
+
+import java.util.Map;
+
+/**
+ * Created by kandreadou on 2/3/15.
+ */
+public class IndexingBolt extends BaseRichBolt {
+    @Override
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+
+    }
+}
